57ba8702dcf34607a5149ca130a608ccdc5458e8
ikhwal <ikhwalsyukur14@gmail.com>
2025-06-18 22:33:01 +0800

    fix : water servo program

--- src/driver/WaterServo.cpp
+++ src/driver/WaterServo.cpp
@@ -1,17 +1,42 @@
 #include   "WaterServo.h"
 #include <esp32Servo.h>
 
-
+Servo servo;
 
 bool WaterServo::begin() 
 {
     pinMode(Servopin, OUTPUT);
+    servo.attach(Servopin);
+    servo.write(90); // Initialize servo to 90 degrees
     return true;
 }
 
 void WaterServo::setAngle(int angle) 
 {
-    Servo servo;
-    servo.attach(Servopin);
-    servo.write(angle);
+    static unsigned long lastCommandTime = 0;
+        if (millis() - lastCommandTime > 500) {
+            lastCommandTime = millis();
+            targetAngle = angle;
+            update(); // Call update to move the servo
+        } 
+}
+
+void WaterServo::update() 
+{
+    unsigned long currentTime = millis();
+        if (currentTime - lastMoveTime >= stepDelay) {
+            if (currentAngle < targetAngle) {
+                currentAngle += stepSize;
+                if (currentAngle > targetAngle) {
+                    currentAngle = targetAngle; // Ensure we don't overshoot
+                }
+            } else if (currentAngle > targetAngle) {
+                currentAngle -= stepSize;
+                if (currentAngle < targetAngle) {
+                    currentAngle = targetAngle; // Ensure we don't undershoot
+                }
+            }
+            servo.write(currentAngle);
+            lastMoveTime = currentTime;
+        }
 }
--- src/driver/WaterServo.h
+++ src/driver/WaterServo.h
@@ -6,10 +6,17 @@ class WaterServo : public WaterServoInterface
 {
 private:
     uint8_t Servopin;
+    int currentAngle = 0;       // Current servo angle
+    int targetAngle = 0;        // Desired target angle
+    int stepSize = 5;           // How many degrees per step
+    unsigned long lastMoveTime = 0;
+    unsigned long stepDelay = 15;
     
 public:
     WaterServo(int pin) : Servopin(pin) {}
     bool begin();
     void setAngle(int angle) override;
+    void update();
+   
     
 };
